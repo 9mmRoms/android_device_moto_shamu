17ba298b7a0b8a9bf9d99670641822e74c1fd5fa
Steven Moreland <smoreland@google.com>
2016-10-10 15:21:07 -0700

    Added light hal.
    Bug: 32022100
    Test: pass
    Change-Id: Ib4b37a86fc278d7ff94d65885ea999f4fd409085
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -220,7 +220,8 @@ PRODUCT_PACKAGES += \
     libion
 
 PRODUCT_PACKAGES += \
-    lights.shamu
+    lights.shamu \
+    android.hardware.light@2.0-impl
 
 # for launcher layout
 #PRODUCT_PACKAGES += \
