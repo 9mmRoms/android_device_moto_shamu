d88be1c25585a2dbd15e939b7f2c2e9201e32c04
Pranav Vashi <neobuddy89@gmail.com>
2017-01-02 21:07:35 +0530

    shamu: Disable Gello and Snap
    Change-Id: I8390ba49f7c8a33d2d67a2523f8db7c7c90db844
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -207,18 +207,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     MotoDoze
 
-# Camera
-PRODUCT_PACKAGES += \
-    Snap
-
-# Snap Config
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.cpp.duplication=false
-
-# Gello
-PRODUCT_PACKAGES += \
-    Gello
-
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     qcom.bluetooth.soc=rome
