823e90fb424aa25220513aa364470e3f381a2f18
Eino-Ville Talvala <etalvala@google.com>
2017-01-19 16:12:42 -0800

    Add necessary camera HAL libraries for passthrough HIDL operation
    Test: No regressions in a full camera CTS pass w/HIDL enabled
          in camara service.
    Bug: 30985004
    Change-Id: Ie7206c322db893eacab0ba8e6647c1587fccca42
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -218,7 +218,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     fmas.spkr_angles=10 \
     fmas.spkr_sgain=0
 
+#CAMERA
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
     libqomx_core \
     libmm-qcamera \
     libmmcamera_interface \
