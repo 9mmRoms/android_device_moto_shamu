24b273b50488ebe7a3c8478860c8f1169e8817f7
elektroschmock <elektroschmock78@googlemail.com>
2017-09-16 23:19:53 -0700

    shamu: Add custom vibrator HAL
    Change-Id: I0b9b29ab5dc2288097b279121e03c81e63811b96
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -344,7 +344,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.shamu
 
 # NFC
 PRODUCT_PACKAGES += \
