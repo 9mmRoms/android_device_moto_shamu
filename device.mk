2199275f4790b060b0f7ce843868e107cf882b32
Pranav Vashi <neobuddy89@gmail.com>
2017-09-19 23:42:12 +0530

    shamu: Add missing face unlock prop
    Change-Id: I82930b154d54b03c3a31ec00cb9b931172bb414a
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -196,7 +196,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.facelock.black_timeout=700 \
     ro.facelock.det_timeout=2500 \
     ro.facelock.rec_timeout=3500 \
-    ro.facelock.est_max_time=600
+    ro.facelock.lively_timeout=3500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
     fmas.spkr_6ch=35,20,110 \
