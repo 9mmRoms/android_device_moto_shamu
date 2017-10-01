8ffadeda108c0fc2a52c888be958c232f8cf0ea7
Pranav Vashi <neobuddy89@gmail.com>
2017-10-01 14:30:10 +0530

    shamu: Add binderized memtrack
    Change-Id: Ideb9c2ce5b493fe8abdea8c4df4795b7c43400a5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -141,6 +141,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     gralloc.msm8084 \
     hwcomposer.msm8084 \
     memtrack.msm8084 \
