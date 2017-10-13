74ca2a14e2d349c3fa68f1b5d3f64273f6e38839
Pranav Vashi <neobuddy89@gmail.com>
2017-10-14 00:37:12 +0530

    healthd: Update
    Change-Id: I4c771c16bb9b596be76eed73ef019eec65241f2c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -4,7 +4,10 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := healthd_board_shamu.cpp
 LOCAL_MODULE := libhealthd.shamu
+LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS := -Werror
 LOCAL_C_INCLUDES := system/core/healthd/include system/core/base/include
+
+LOCAL_STATIC_LIBRARIES := libcutils
 
 include $(BUILD_STATIC_LIBRARY)
