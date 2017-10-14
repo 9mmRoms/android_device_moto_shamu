cd08d7a13fa24a11277f5038a32eb80bffea62ed
Pranav Vashi <neobuddy89@gmail.com>
2017-10-14 17:42:21 +0530

    shamu: Remove additional offload props
    Change-Id: I1a27d3582b4061055a9ac1e12164a0e764ec118f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -443,12 +443,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.fluence.voicecall=true \
     persist.audio.fluence.voicecomm=false \
     persist.audio.fluence.voicerec=false \
-    persist.audio.fluence.speaker=false \
-    audio.offload.buffer.size.kb=32 \
-    audio.offload.gapless.enabled=true \
-    audio.offload.video=true \
-    audio.offload.pcm.16bit.enable=true \
-    audio.offload.pcm.24bit.enable=true
+    persist.audio.fluence.speaker=false
 
 # OEM Unlock reporting
 ADDITIONAL_DEFAULT_PROPERTIES += \
